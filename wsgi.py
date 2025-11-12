73bfb6fc993fc31ce7ec32c6cb788a5b770b76f8
Catwomen777 <raushawillis336@gmail.com>
2025-11-11 23:27:26 -0800

    Fix app reference for Gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 from app import create_app
 from app.models import db
 
-def app():
-    flask_app = create_app('ProductionConfig')
-    with flask_app.app_context():
-        db.create_all()
-    return flask_app
+app = create_app('ProductionConfig')
+
+with app.app_context():
+    db.create_all()
