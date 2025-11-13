9cdcc8c07a0bccb6b705b47da641944865af70bd
Catwomen777 <raushawillis336@gmail.com>
2025-11-12 17:16:37 -0800

    Fix config_name NameError for Render deployment

--- app/__init__.py
+++ app/__init__.py
@@ -16,9 +16,9 @@ swagger_bp = get_swaggerui_blueprint(
 
 
 
-def create_app(config_name="ProductionConfig"):
-    app = Flask(__name__)
-    app.config.from_object(f'config.{config_name}')
+def create_app(config_name="DevelopmentConfig"):
+    app = Flask(__name__, static_url_path='/static', static_folder='app/static')
+    app.config.from_object(f"config.{config_name}")
     
     db.init_app(app)
     ma.init_app(app)
--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,10 @@
 from app import create_app
 from app.models import db
 
-app = create_app('ProductionConfig')
+from app import create_app
 
-with app.app_context():
-     db.create_all()
-     
-    
+app = create_app("ProductionConfig")
+
+if __name__ == "__main__":
+    app.run()
+
