90750258d82d1c3b83a779efbcfe67be17c2073d
Catwomen777 <raushawillis336@gmail.com>
2025-11-12 17:04:11 -0800

    changed

--- app/__init__.py
+++ app/__init__.py
@@ -19,8 +19,7 @@ swagger_bp = get_swaggerui_blueprint(
 def create_app(config_name="DevelopmentConfig"):
 
     
-    app = Flask(__name__, static_url_path='/static', static_folder='app/static')
-
+   
     app.config.from_object(f'config.{config_name}')
     
     db.init_app(app)
